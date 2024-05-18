e5d50aa96c30e6287c11a8a36dbc9d185c7b6a02
llerga12 <llerga@veleri.hr>
2024-05-18 11:21:14 +0200

    Inital comit

---
+++ zadatak/src/zadatak/GlavniProzor.java
@@ -0,0 +1,56 @@
+package zadatak;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GlavniProzor {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GlavniProzor window = new GlavniProzor();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GlavniProzor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Tipka1");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DlgProzor1 dlg = new DlgProzor1();
+				dlg.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(10, 11, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
